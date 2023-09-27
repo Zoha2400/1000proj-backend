4c5076de4ea34e705009dac4dc576ca3d4054986
Zoha2400 <vasv29834@gmail.com>
2023-09-28 00:42:12 +0500

    local data

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World !');
+
+//basic local data
+const projects = [
+    {id: 1, area: '21x23', acres: '6', style: 'newModern', cons: '7.000.000', data:'smth!'},
+    {id: 2, area: '21x23', acres: '6', style: 'newModern', cons: '7.000.000', data:'smth!'},
+    {id: 3, area: '21x23', acres: '6', style: 'newModern', cons: '7.000.000', data:'smth!'},
+    {id: 4, area: '21x23', acres: '6', style: 'newModern', cons: '7.000.000', data:'smth!'},
+    {id: 5, area: '21x23', acres: '6', style: 'newModern', cons: '7.000.000', data:'smth!'},
+    {id: 6, area: '21x23', acres: '6', style: 'newModern', cons: '7.000.000', data:'smth!'},
+    {id: 7, area: '21x23', acres: '6', style: 'newModern', cons: '7.000.000', data:'smth!'},
+    {id: 8, area: '21x23', acres: '6', style: 'newModern', cons: '7.000.000', data:'smth!'},
+    {id: 9, area: '21x23', acres: '6', style: 'newModern', cons: '7.000.000', data:'smth!'},
+    {id: 10, area: '21x23', acres: '6', style: 'newModern', cons: '7.000.000', data:'smth!'},
+    {id: 11, area: '21x23', acres: '6', style: 'newModern', cons: '7.000.000', data:'smth!'},
+    {id: 12, area: '21x23', acres: '6', style: 'newModern', cons: '7.000.000', data:'smth!'},
+    {id: 13, area: '21x23', acres: '6', style: 'newModern', cons: '7.000.000', data:'smth!'},
+]
+
+app.get('/api/get_all', (req, res) => {
+    res.json(projects);
 })
 
 app.listen(PORT, () => {
